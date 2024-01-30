f30fa15ad37a089e177ff590159185905cdf0023
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-01-30 21:13:20 +0500

    Add Course.java

---
+++ Lab # 2/Course.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Course {
+
+    public String CourseName;
+    public int CreditHours;
+
+    public Course() {
+    
+    }
+
+    public void Operation1() {
+    
+    }
+
+    public void setCourseDetails() {
+    
+    }
+
+}
