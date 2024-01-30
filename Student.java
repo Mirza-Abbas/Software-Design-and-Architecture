b456d6874f7f043566ebb849de7e3e6ed3706262
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-01-30 21:13:39 +0500

    Add Student.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Student {
+    
+    public String FirstName;
+    public String LastName;
+    public int Batch;
+    public String RollNo;
+    public String Section;
+
+    public Student() {
+
+    }
+
+    
+    public void setStudentDetails() {
+        
+    }
+
+}
