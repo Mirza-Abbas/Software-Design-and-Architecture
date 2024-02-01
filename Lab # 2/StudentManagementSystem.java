70b298679a423d75190c5c45bb86a1e731fed447
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-02-01 14:15:25 +0500

    Add StudentManagementSystem.java

---
+++ Lab # 2/StudentManagementSystem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class StudentManagementSystem {
+
+    public StudentManagementSystem() {
+    
+    }
+
+    private String StudentName;
+    private String Course;
+
+    public String getStudentDetails() {
+
+        return "";
+    }
+
+    public String SelectCourse() {
+        
+        return "";
+    }
+
+}
